1aba5072626d6eb63e51e9ff9ffc5c77ab3188f1
Najmul Huda <najmul.myself@gmail.com>
2021-03-02 00:51:36 +0600

    make code more easier with func

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
-let genRandomNumber = document.getElementById('random-number');
-let genBtn = document.querySelector(".generate-btn");
-let clickInput = document.getElementById('clickInput');
+// let genRandomNumber = document.getElementById('random-number');
+// let genBtn = document.querySelector(".generate-btn");
+// let clickInput = document.getElementById('clickInput');
 
-// getAll Number
-let num0 = document.getElementById('num-0');
-let num1 = document.getElementById('num-1');
-let num2 = document.getElementById('num-2');
-let num3 = document.getElementById('num-3');
-let num4 = document.getElementById('num-4');
-let num5 = document.getElementById('num-5');
-let num6 = document.getElementById('num-6');
-let num7 = document.getElementById('num-7');
-let num8 = document.getElementById('num-8');
-let num9 = document.getElementById('num-9');
+// // getAll Number
+// let num0 = document.getElementById('num-0');
+// let num1 = document.getElementById('num-1');
+// let num2 = document.getElementById('num-2');
+// let num3 = document.getElementById('num-3');
+// let num4 = document.getElementById('num-4');
+// let num5 = document.getElementById('num-5');
+// let num6 = document.getElementById('num-6');
+// let num7 = document.getElementById('num-7');
+// let num8 = document.getElementById('num-8');
+// let num9 = document.getElementById('num-9');
 
-let test = Math.ceil(Math.random() * 10000);
-// console.log(test)
-genBtn.addEventListener('click',function(){
-    genRandomNumber.value = test;
-});
+// let test = Math.ceil(Math.random() * 10000);
+// // console.log(test)
+// genBtn.addEventListener('click',function(){
+//     genRandomNumber.value = test;
+// });
 
-let zero = num0.innerText;
-let one = num1.innerText;
-let two = num2.innerText;
-let three = num3.innerText;
-let four = num4.innerText;
-let five = num5.innerText;
-let six = num6.innerText;
-let seven = num7.innerText;
-let eight = num8.innerText;
-let nine = num9.innerText;
+// let zero = num0.innerText;
+// let one = num1.innerText;
+// let two = num2.innerText;
+// let three = num3.innerText;
+// let four = num4.innerText;
+// let five = num5.innerText;
+// let six = num6.innerText;
+// let seven = num7.innerText;
+// let eight = num8.innerText;
+// let nine = num9.innerText;
 
-let aText = clickInput.value;
-// function addMultipleKeyNumber(){
-//     console.log('Hello')
+// let aText = clickInput.value;
+// // function addMultipleKeyNumber(){
+// //     console.log('Hello')
+// // }
+// let addMultipleKeyNumber =() => {
+//     // console.log(numberKey)
+//     clickInput.value = aText + num0.innerText;
+//     console.log('this text',aText)
 // }
-let addMultipleKeyNumber =() => {
-    // console.log(numberKey)
-    clickInput.value = aText + num0.innerText;
-    console.log('this text',aText)
-}
-num0.addEventListener('click',addMultipleKeyNumber);
-num1.addEventListener('click', addMultipleKeyNumber);
+// num0.addEventListener('click',addMultipleKeyNumber);
+// num1.addEventListener('click', addMultipleKeyNumber);
 // num2.addEventListener('click', addMultipleKeyNumber(two));
 // num3.addEventListener('click', addMultipleKeyNumber(three));
 // num4.addEventListener('click', addMultipleKeyNumber(four));
@@ -51,3 +51,20 @@ num1.addEventListener('click', addMultipleKeyNumber);
 // num8.addEventListener('click', addMultipleKeyNumber(eight));
 // num9.addEventListener('click', addMultipleKeyNumber(nine));
 
+// Making Code Clear with Function
+
+function getRandomNum(){
+    let randomPin = Math.random() * 10000;
+    let getPin = (randomPin + '').split('.')[0];
+    if(getPin.length === 4 ){
+        return getPin;
+    }
+    else{
+        return getRandomNum()
+    }
+}
+
+function generatePin(){
+    let inputRandomNumber = document.getElementById('random-number');
+    inputRandomNumber.value = getRandomNum();
+}
